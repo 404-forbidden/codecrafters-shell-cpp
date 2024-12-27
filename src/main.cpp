2bdae0c1a908052f7b4a6a738f4b8cf3e3891ae6
Seonwoo <nsw6662@gmail.com>
2024-12-27 15:45:35 +0900

    REPL

--- src/main.cpp
+++ src/main.cpp
@@ -5,10 +5,12 @@ int main() {
   std::cout << std::unitbuf;
   std::cerr << std::unitbuf;
 
-  // Uncomment this block to pass the first stage
-  std::cout << "$ ";
+  while(true) {
+      // Uncomment this block to pass the first stage
+      std::cout << "$ ";
 
-  std::string input;
-  std::getline(std::cin, input);
-  std::cout << input << ": command not found" << std::endl;
+      std::string input;
+      std::getline(std::cin, input);
+      std::cout << input << ": command not found" << std::endl;
+  }
 }
