6362594d153704e948f6228a0231675a327d9c00
Seonwoo <nsw6662@gmail.com>
2024-12-27 16:03:55 +0900

    The exit builtin

--- src/main.cpp
+++ src/main.cpp
@@ -5,12 +5,23 @@ int main() {
   std::cout << std::unitbuf;
   std::cerr << std::unitbuf;
 
+  // REPL (Read-Eval-Print Loop)
   while(true) {
       // Uncomment this block to pass the first stage
       std::cout << "$ ";
 
       std::string input;
       std::getline(std::cin, input);
+
+      // exit
+      if (input.substr(0, 4) == "exit") {
+          size_t spacePos = input.find(' ');
+          if (spacePos != std::string::npos) {
+              std::string exitCode = input.substr(spacePos + 1);
+              exit(std::stoi(exitCode));
+          }
+      }
+
       std::cout << input << ": command not found" << std::endl;
   }
 }
