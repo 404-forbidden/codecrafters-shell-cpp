396cae6b17ec6a0f0c57d422bf725d4f8634b4a8
Seonwoo <nsw6662@gmail.com>
2024-12-29 15:30:14 +0900

    Single quotes

--- src/main.cpp
+++ src/main.cpp
@@ -34,20 +34,45 @@ std::string find_executable(const std::string& cmd) {
     return "";
 }
 
-std::vector<std::string> split_command(const std::string& input) {
+std::vector<std::string> parse_command(const std::string& input) {
     std::vector<std::string> args;
-    std::stringstream ss(input);
-    std::string arg;
+    std::string current_arg;
+    bool in_quotes = false;
+    char quote_char = 0;
 
-    while (ss >> arg) {
-        args.push_back(arg);
+    for (size_t i = 0; i < input.length(); i++) {
+        char c = input[i];
+
+        if ((c == '\'' || c == '"') && (!in_quotes || c == quote_char)) {
+            if (in_quotes) {
+                in_quotes = false;
+                quote_char = 0;
+            } else {
+                in_quotes = true;
+                quote_char = c;
+            }
+            continue;
+        }
+
+        if (std::isspace(c) && !in_quotes) {
+            if (!current_arg.empty()) {
+                args.push_back(current_arg);
+                current_arg.clear();
+            }
+        } else {
+            current_arg += c;
+        }
+    }
+
+    if (!current_arg.empty()) {
+        args.push_back(current_arg);
     }
 
     return args;
 }
 
 void execute_external_command(const std::string& input) {
-    std::vector<std::string> args = split_command(input);
+    std::vector<std::string> args = parse_command(input);
     if (args.empty()) return;
 
     std::string program_path = find_executable(args[0]);
@@ -81,31 +106,29 @@ int main() {
     // commands
     std::map<std::string, std::function<void(const std::string &)>> commands = {
             {"exit", [](const std::string &input) {
-                size_t pos = input.find(' ');
-                if (pos != std::string::npos) {
-                    std::string exitCode = input.substr(pos + 1);
-                    exit(std::stoi(exitCode));
+                auto args = parse_command(input);
+                if (args.size() > 1) {
+                    exit(std::stoi(args[1]));
                 }
+                exit(0);
             }},
             {"echo", [](const std::string &input) {
                 size_t pos = input.find(' ');
                 if (pos != std::string::npos) {
-                    std::string text = input.substr(pos + 1);
-                    std::cout << text << std::endl;
+                    std::cout << input.substr(pos + 1) << std::endl;
                 }
             }},
             {"type", [&commands](const std::string& input) {
-                size_t pos = input.find(' ');
-                if (pos != std::string::npos) {
-                    std::string cmd = input.substr(pos + 1);
-                    if (commands.count(cmd)) {
-                        std::cout << cmd << " is a shell builtin" << std::endl;
+                auto args = parse_command(input);
+                if (args.size() > 1) {
+                    if (commands.count(args[1])) {
+                        std::cout << args[1] << " is a shell builtin" << std::endl;
                     } else {
-                        std::string path = find_executable(cmd);
+                        std::string path = find_executable(args[1]);
                         if (!path.empty()) {
-                            std::cout << cmd << " is " << path << std::endl;
+                            std::cout << args[1] << " is " << path << std::endl;
                         } else {
-                            std::cout << cmd << ": not found" << std::endl;
+                            std::cout << args[1] << ": not found" << std::endl;
                         }
                     }
                 }
@@ -121,29 +144,23 @@ int main() {
             // change directory
             {"cd", [](const std::string &input) {
                 const char* home = std::getenv("HOME");
-                size_t pos = input.find(' ');
+                auto args = parse_command(input);
                 std::string path;
 
-                if (pos != std::string::npos) {
-                    path = input.substr(pos + 1);
-
+                if (args.size() > 1) {
+                    path = args[1];
                     // start with '~'
-                    if (path[0] == '~') {
-                        if (home != nullptr) {
-                            if (path == "~") {
-                                path = home;
-                            }
-                            else {
-                                path = std::string(home) + path.substr(1);
-                            }
+                    if (path[0] == '~' && home != nullptr) {
+                        if (path == "~") {
+                            path = home;
+                        } else {
+                            path = std::string(home) + path.substr(1);
                         }
                     }
                 }
                 // without argument
-                else {
-                    if (home != nullptr) {
-                        path = home;
-                    }
+                else if (home != nullptr) {
+                    path = home;
                 }
 
                 try {
@@ -161,7 +178,12 @@ int main() {
         std::string input;
         std::getline(std::cin, input);
 
-        std::string cmd = input.substr(0, input.find(' '));
+        auto args = parse_command(input);
+        if (args.empty()) {
+            continue;
+        }
+
+        std::string cmd = args[0];
         if (commands.count(cmd)) {
             commands[cmd](input);
         }
